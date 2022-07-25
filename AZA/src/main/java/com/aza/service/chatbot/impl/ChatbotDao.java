02a0c468b7a5f749d954944d03ae791625d93410
kyungho <akh1216@gmail.com>
2022-07-25 10:03:16 +0900

    2022_07_25 10:00

--- AZA/src/main/java/com/aza/service/chatbot/impl/ChatbotDao.java
+++
@@ -1,9 +0,0 @@
-package com.aza.service.chatbot.impl;
-
-public class ChatbotDao {
-
-	public ChatbotDao() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- AZA/src/main/java/com/aza/service/chatbot/impl/ChatbotService.java
+++
@@ -1,9 +0,0 @@
-package com.aza.service.chatbot.impl;
-
-public class ChatbotService {
-
-	public ChatbotService() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
